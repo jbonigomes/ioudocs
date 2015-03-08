a1afdb9d61d2c61740e67e1678809507278289f1
Jose B Gomes <jbonigomes@yahoo.com.br>
2015-03-08 13:53:24 +0000

    Fixes for references

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,9 @@
 (function() {
 
-  $('.year').html((new Date).getFullYear());
+  fullyear = moment().format('YYYY');
+  fulldate = moment().format('dddd, Do MMM, YYYY');
+
+  $('.year').html(fullyear);
+  $('.fulldate').html(fulldate);
 
 })();
